f12fa4538ec44bfcaff9bcb9ef98454ec26723bb
Cliff Wu <z800201002005@gmail.com>
2023-10-24 17:14:57 +0900

    Formatting Java code

--- android/app/src/main/java/com/reactnativesample/MainActivity.java
+++ android/app/src/main/java/com/reactnativesample/MainActivity.java
@@ -17,15 +17,13 @@ public class MainActivity extends ReactActivity {
   }
 
   /**
-   * Returns the instance of the {@link ReactActivityDelegate}. Here we use a util class {@link
-   * DefaultReactActivityDelegate} which allows you to easily enable Fabric and Concurrent React
-   * (aka React 18) with two boolean flags.
+   * Returns the instance of the {@link ReactActivityDelegate}. Here we use a util class
+   * {@link DefaultReactActivityDelegate} which allows you to easily enable Fabric and Concurrent
+   * React (aka React 18) with two boolean flags.
    */
   @Override
   protected ReactActivityDelegate createReactActivityDelegate() {
-    return new DefaultReactActivityDelegate(
-        this,
-        getMainComponentName(),
+    return new DefaultReactActivityDelegate(this, getMainComponentName(),
         // If you opted-in for the New Architecture, we enable the Fabric Renderer.
         DefaultNewArchitectureEntryPoint.getFabricEnabled());
   }
